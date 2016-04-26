346022f7a5c1063385c980667169e5f8f2f3373b
Warodom Khamphanchai <kwarodom@vt.edu>
2016-04-26 17:46:49 -0400

    complete create quiz with angularJS lynda.com

--- js/quiz.js
+++ js/quiz.js
@@ -1,5 +1,70 @@
 (function(){
 
-	// script goes here
+	var app=angular.module('myQuiz',[]);
+    app.controller('QuizController',['$scope', '$http', '$sce', function ($scope,$http,$sce){
+        
+        $scope.score = 0;
+        $scope.activeQuestion = -1;
+        $scope.activeQuestionAnswered = 0; 
+        $scope.percentage = 0;
+        
+        $http.get('quiz_data.json').then(function (quizData) {
+            $scope.myQuestions = quizData.data; 
+            $scope.totalQuestions = $scope.myQuestions.length;
+        });
+        
+        $scope.selectAnswer = function (qIndex, aIndex) {
+            // alert('qIndex' + qIndex + 'aIndex' + aIndex); //for testing
+            var questionState = $scope.myQuestions[qIndex].questionState;
+            
+            if( questionState != 'answered' ){
+                $scope.myQuestions[qIndex].selectedAnswer = aIndex;
+                var correctAnswer = $scope.myQuestions[qIndex].correct;
+                $scope.myQuestions[qIndex].correctAnswer = correctAnswer;
+                
+                if (aIndex === correctAnswer) {
+                    $scope.myQuestions[qIndex].correctness = 'correct';
+                    $scope.score += 1;
+                } else {
+                    $scope.myQuestions[qIndex].correctness = 'incorrect';
+                }
+
+                $scope.myQuestions[qIndex].questionState = 'answered';
+            }
+            
+            $scope.percentage = (($scope.score / $scope.totalQuestions)*100).toFixed(1);
+            
+        }
+        
+        $scope.isSelected = function(qIndex, aIndex){
+            return $scope.myQuestions[qIndex].selectedAnswer === aIndex;
+        }
+
+        $scope.isCorrect = function(qIndex, aIndex){
+            return $scope.myQuestions[qIndex].correctAnswer === aIndex;
+        }
+        
+        $scope.selectContinue = function(){
+            return $scope.activeQuestion += 1;
+        }
+        
+        $scope.createShareLink = function (percentage) {
+            var url = 'http://google.com';
+            var emailLink = '<a class="btn email" href="mailto:?subject=Try to beat my quiz score!&amp;body=I scored a '+percentage+'% on this quiz about Saturn. Try to beat my score at '+url+'">Email a friend</a>';
+            var twitterLink = '<a class="btn twitter" target="_blank" href="http://twitter.com/share?text=I scored a '+percentage+'% on this quiz about Saturn. Try to beat my score at&amp;hashtags=SaturnQuiz&amp;url='+url+'">Tweet your score</a>';
+            var facebookLink = '<a class="btn facebook" target="_blank" href="http://www.facebook.com/dialog/feed?app_id=599703650211336&link=http://developers.facebook.com/docs/reference/dialogs/' +
+                '&picture=http://fbrell.com/f8.jpg' +
+                '&name=Saturn%20Quiz!' +
+                '&caption=This%20is%20so%20cool%20man!' +
+                '&description=I scored a '+percentage+'% on this quiz about Saturn. Try to beat my score at '+url+'' +
+                '&message=Facebook%20Dialogs%20are%20so%20easy! test dsadas d dsadsdsaad">Share your score</a>';
+            
+            var newMarkup = emailLink + twitterLink + facebookLink;
+            
+            return $sce.trustAsHtml(newMarkup);
+        }
+        
+    }]);
+    
 
 })();
